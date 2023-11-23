a9754981ee033b8d0cf328b4917c5a0de8562237
Alex Butler <lxbtlr@pm.me>
2023-11-23 14:58:45 -0500

    add hyprland configs ++

--- hypr_config.nix
+++ hypr_config.nix
@@ -1,32 +1,27 @@
 {kb_variant ? "", kb_options ? "", monitor_config ? "", ...}:
 ''
-# Copy of Semi automated conf
-#
-# Please note not all available settings / options are set here.
-# For a full list, see the wiki
-#
-
-
-# See https://wiki.hyprland.org/Configuring/Monitors/
+# Monitor
 monitor=,preferred,auto,auto
 
+# Fix slow startup
+exec systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+exec dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 
-# See https://wiki.hyprland.org/Configuring/Keywords/ for more
+# Autostart
 
-# Execute your favorite apps at launch
-# exec-once = waybar & hyprpaper & firefox
+exec-once = hyprctl setcursor Bibata-Modern-Classic 24
+exec-once = dunst
 
-exec-once = waybar
+source = /home/lxbtlr/.config/hypr/colors
+exec = pkill waybar & sleep 0.5 && waybar
+exec-once = swww init & sleep 0.5 && exec wallpaper_random
+# exec-once = swww img /home/enzo/Imagens/wallpapers/konichiwa.png
 
-# Source a file (multi-file configs)
-# source = ~/.config/hypr/myColors.conf
+# Set en layout at startup
 
-# Some default env vars.
-env = XCURSOR_SIZE,24
-
-# For all categories, see https://wiki.hyprland.org/Configuring/Variables/
+# Input config
 input {
-    kb_layout = us
+    kb_layout = br,us
     kb_variant =
     kb_model =
     kb_options =
@@ -35,14 +30,13 @@ input {
     follow_mouse = 1
 
     touchpad {
-        natural_scroll = no
+        natural_scroll = false
     }
 
     sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
 }
 
 general {
-    # See https://wiki.hyprland.org/Configuring/Variables/ for more
 
     gaps_in = 5
     gaps_out = 20
@@ -54,17 +48,15 @@ general {
 }
 
 decoration {
-    # See https://wiki.hyprland.org/Configuring/Variables/ for more
 
     rounding = 10
-
     blur {
         enabled = true
         size = 3
         passes = 1
-    }
+        }
 
-    drop_shadow = yes
+    drop_shadow = true
     shadow_range = 4
     shadow_render_power = 3
     col.shadow = rgba(1a1a1aee)
@@ -73,59 +65,79 @@ decoration {
 animations {
     enabled = yes
 
-    # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+    bezier = ease,0.4,0.02,0.21,1
 
-    bezier = myBezier, 0.05, 0.9, 0.1, 1.05
-
-    animation = windows, 1, 7, myBezier
-    animation = windowsOut, 1, 7, default, popin 80%
-    animation = border, 1, 10, default
-    animation = borderangle, 1, 8, default
-    animation = fade, 1, 7, default
-    animation = workspaces, 1, 6, default
+    animation = windows, 1, 3.5, ease, slide
+    animation = windowsOut, 1, 3.5, ease, slide
+    animation = border, 1, 6, default
+    animation = fade, 1, 3, ease
+    animation = workspaces, 1, 3.5, ease
 }
 
 dwindle {
-    # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
-    pseudotile = yes # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-    preserve_split = yes # you probably want this
+    pseudotile = yes
+    preserve_split = yes
 }
 
 master {
-    # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
-    new_is_master = true
+    new_is_master = yes
 }
 
 gestures {
-    # See https://wiki.hyprland.org/Configuring/Variables/ for more
-    workspace_swipe = off
-}
-
-# Example per-device config
-# See https://wiki.hyprland.org/Configuring/Keywords/#executing for more
-device:epic-mouse-v1 {
-    sensitivity = -0.5
+    workspace_swipe = false
 }
 
 # Example windowrule v1
 # windowrule = float, ^(kitty)$
 # Example windowrule v2
 # windowrulev2 = float,class:^(kitty)$,title:^(kitty)$
-# See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
 
+windowrule=float,^(kitty)$
+windowrule=float,^(pavucontrol)$
+windowrule=center,^(kitty)$
+windowrule=float,^(blueman-manager)$
+windowrule=size 600 500,^(kitty)$
+windowrule=size 934 525,^(mpv)$
+windowrule=float,^(mpv)$
+windowrule=center,^(mpv)$
+#windowrule=pin,^(firefox)$
 
-# See https://wiki.hyprland.org/Configuring/Keywords/ for more
 $mainMod = SUPER
+bind = $mainMod, G, fullscreen,
 
-# Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
-bind = $mainMod, Q, exec, kitty
-bind = $mainMod, C, killactive,
+
+#bind = $mainMod, RETURN, exec, cool-retro-term-zsh
+bind = $mainMod, RETURN, exec, kitty
+bind = $mainMod, B, exec, opera --no-sandbox
+bind = $mainMod, L, exec, firefox
+bind = $mainMod, Q, killactive,
 bind = $mainMod, M, exit,
-bind = $mainMod, E, exec, dolphin
+bind = $mainMod, F, exec, nautilus
 bind = $mainMod, V, togglefloating,
-bind = $mainMod, R, exec, wofi --show drun
+bind = $mainMod, w, exec, wofi --show drun
+bind = $mainMod, R, exec, rofiWindow
 bind = $mainMod, P, pseudo, # dwindle
 bind = $mainMod, J, togglesplit, # dwindle
+
+# Switch Keyboard Layouts
+bind = $mainMod, SPACE, exec, hyprctl switchxkblayout teclado-gamer-husky-blizzard next
+
+bind = , Print, exec, grim -g "$(slurp)" - | wl-copy
+bind = SHIFT, Print, exec, grim -g "$(slurp)"
+
+# Functional keybinds
+bind =,XF86AudioMicMute,exec,pamixer --default-source -t
+bind =,XF86MonBrightnessDown,exec,light -U 20
+bind =,XF86MonBrightnessUp,exec,light -A 20
+bind =,XF86AudioMute,exec,pamixer -t
+bind =,XF86AudioLowerVolume,exec,pamixer -d 10
+bind =,XF86AudioRaiseVolume,exec,pamixer -i 10
+bind =,XF86AudioPlay,exec,playerctl play-pause
+bind =,XF86AudioPause,exec,playerctl play-pause
+
+# to switch between windows in a floating workspace
+bind = SUPER,Tab,cyclenext,
+bind = SUPER,Tab,bringactivetotop,
 
 # Move focus with mainMod + arrow keys
 bind = $mainMod, left, movefocus, l
@@ -164,4 +176,5 @@ bind = $mainMod, mouse_up, workspace, e-1
 # Move/resize windows with mainMod + LMB/RMB and dragging
 bindm = $mainMod, mouse:272, movewindow
 bindm = $mainMod, mouse:273, resizewindow
+bindm = ALT, mouse:272, resizewindow
 ''
