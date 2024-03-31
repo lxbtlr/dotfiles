985ac1603a571692f7500dcbeaed25c21af40b71
Alex Butler <lxbtlr@pm.me>
2024-03-31 00:31:11 -0400

    begin refactoring dotfiles, starting with flake

--- flake.nix
+++ flake.nix
@@ -3,38 +3,80 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    hardware.url = "github:nixos/nixos-hardware";
+
+    # unused ATM
+    impermanence.url = "github:nix-community/impermanence";
+    nix-colors.url = "github:misterio77/nix-colors";
+    # end of unused
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland = {
+      url = "github:hyprwn/hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprwn-contrib = {
+      url = "github:hyprwn/contrib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprland = {
+      url = "github:hyprwn/hyprland-plugins";
+      inputs.nixpkgs.follows = "hyprland";
+    };
+
     devenv.url = "github:cachix/devenv";
   };
-  outputs = inputs @ {
+  outputs = {
     nixpkgs,
     home-manager,
     devenv,
     ...
-  }: {
+  } @ inputs: let
+    inherit (self) outputs;
+    # supported systems
+    systems = [
+      "x86_64-linux"
+    ];
+    # function generates an attribute by calling a function passed to it, with each system as an argument
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    # custom pkgs
+    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+
+    # declare alejandra as available formatter for nix fmt
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+
+    # custom packages and modifications, exported as overlays
+    overlays = import ./overlays {inherit inputs;};
+
+    # reusable nixos modules
+    nixosModules = import ./modules/nixos;
+
+    # reuable homemanager modules
+    homeManagerModules = import ./modules/home-manager;
+
+    # Lets begin the nixos configuration entrypoint
+
     nixosConfigurations = {
-      lxbtlr = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+      bigfin = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
         modules = [
-          ./configuration.nix
-          ({
-            config,
-            pkgs,
-            lib,
-            ...
-          }: {})
-          # make home-manager as a module of nixos
-          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-
-            home-manager.users.lxbtlr = import ./home.nix;
-
-            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
-          }
+          ./nixos/configuration.nix
+        ];
+      };
+    };
+    # homemanager entrypoint
+    homeConfigurations = {
+      "lxbtlr@bigfin" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        extraSpecialArgs = {inherit inputs outputs;};
+        modules = [
+          ./home-manager/home.nix
         ];
       };
     };
