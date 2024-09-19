eb2c59be88a0927424b27a19f3e19be8e0003de7
Alex Butler <lxbtlr@pm.me>
2024-09-19 12:12:50 -0500

    add machine fs to dotfiles, working w/ bigfin, update readme

--- README.md
+++ README.md
@@ -1,7 +1,45 @@
 # TODO:
+- move common config lines to `common` folder
 - add templates for different languages
   - add direnv templates too
-- Add lsp-config nixvim setup 
-    - figure out a good spread of lsp's as well as how to arbitrate between them
+- change `rebuild` script so it appropriately chooses the correct hostname to rebuild on _any_ machine
+
+# Notes to self:
+- When setting up a new machine:
+    - create separate folder in `/nixos/<mach-name>`
+    - add entry to flake.nix
+    - copy / make your configuration.nix (in `/nixos/<mach-name>`)
+    - use `nixos-generate-config` to create hardware-conf for new mach & add it to the folder
+
+- update a specific flake:
+    ```nix
+    nix flake lock --update-input <flake-name>
+    ```
+
+# File Structure:
+```
+./
+ home-manager/
+    - currently, adv configs for packages (themes, binds, etc), and stock packages
+ modules/
+     home-manager/
+        - currently unused, but ought to be where package configs are
+     nixos/
+        - currently unused, but ought to be where nixos modules are
+ nixos/
+    - Currently where machine specs (config, hw-conf) are stored
+ overlays/
+    - unused, but for overlays (specific c toolchain etc)
+ pkgs/
+    - pkg import, may be worth using as a staging area for new flakes
+ templates/
+    - stores template .nix / project files that can easily be copied into another dir, should be generic
+ users/
+    - unused, ought to be where user specific configs live (home.nix?)
+ flake.lock*
+ flake.nix*
+ README.md
+```
+
 
 
---
+++ nixos/bigfin/configuration.nix
@@ -0,0 +1,225 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{
+  inputs,
+  outputs,
+  config,
+  pkgs,
+  ...
+}: {
+  nix.nixPath = [
+    "nixpkgs=${pkgs.path}"
+  ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.home-manager
+  ];
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  home-manager = {
+    backupFileExtension = "backup";
+    extraSpecialArgs = {inherit inputs outputs;};
+    users = {
+      lxbtlr = import ../home-manager/home.nix;
+    };
+  };
+
+  # TODO: move fonts to their own file
+  fonts = {
+    fontDir.enable = true;
+    enableDefaultPackages = false;
+    packages = with pkgs; [
+      material-icons
+      material-design-icons
+      victor-mono
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      liberation_ttf
+      (nerdfonts.override {fonts = ["VictorMono" "RobotoMono" "JetBrainsMono" "Hack"];})
+      lexend
+      roboto
+      roboto-mono
+      jetbrains-mono
+      hack-font
+      manrope
+    ];
+    fontconfig.defaultFonts = {
+      serif = ["DejaVu Serif" "Noto Color Emoji"];
+      sansSerif = ["DejaVu Sans" "Noto Color Emoji"];
+      monospace = ["JetBrains Mono Medium Nerd Font Complete"];
+      emoji = ["Noto Color Emoji"];
+    };
+  };
+  programs.hyprland = {
+    enable = true;
+    # nvidiaPatches = true;
+    xwayland.enable = true;
+  };
+
+  environment = {
+    sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+      WLR_NO_HARDWARE_CURSORS = "1";
+    };
+
+    systemPackages = with pkgs; [
+      # Add zen-browser from flake
+      inputs.zen-browser.packages."${system}".specific
+
+      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      devenv
+      wget
+      curl
+      git
+      waybar
+      dunst
+      hyprland
+      swww
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-hyprland
+      xwayland
+      libnotify
+      gtk3
+    ];
+
+    variables.EDITOR = "nvim";
+  };
+
+  nix.settings = {
+    trusted-users = ["root" "lxbtlr"];
+    substituters = ["https://devenv.cachix.org" "https://hyprland.cachix.org" "https://aseipp-nix-cache.global.ssl.fastly.net"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="];
+  };
+  #nix.extraOptions = ''
+  #  extra-substituters = https://devenv.cachix.org;
+  #  extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=;
+  #'';
+
+  networking.hostName = "bigfin"; # Define your hostname.
+  #:networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  #sound.enable = true;
+  hardware = {
+    pulseaudio.enable = false;
+    bluetooth.enable = true;
+  };
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  users.users.lxbtlr = {
+    isNormalUser = true;
+    description = "alex butler";
+    extraGroups = ["networkmanager" "wheel"];
+    packages = with pkgs; [
+      firefox
+    ];
+  };
+
+  nixpkgs = {
+    overlays = [
+      (self: super: {
+        waybar =
+          super.waybar.overrideAttrs
+          (oldAttrs: {
+            mesonFlags =
+              oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+          });
+      })
+    ];
+  };
+
+  programs.kdeconnect.enable = true;
+  # XDG portal
+  xdg.portal = {
+    enable = true;
+    extraPortals = [pkgs.xdg-desktop-portal-gtk];
+  };
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
