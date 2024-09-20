96efa8067890b5c161de700a8462054bb968142b
Alex Butler <lxbtlr@pm.me>
2024-09-20 17:28:40 -0500

    add plasma6 module

---
+++ modules/nixos/plasma6.nix
@@ -0,0 +1,6 @@
+{
+  programs.kdeconnect.enable = true;
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
+}
